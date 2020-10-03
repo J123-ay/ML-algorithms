866c1977f8878712a05eee373fb5fd179fca4a16
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-10-03 13:26:46 +0530

    Change the train -test split
    Change the train -test split  so that the model doesn't get overfitting

--- REGRESSION/Linear Regression.py
+++ REGRESSION/Linear Regression.py
@@ -13,7 +13,7 @@ Y=dataset.iloc[:,-1].values
 print(Y)
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,random_state=0)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
 print(X_train)
 print(X_test)
 print(Y_train)
